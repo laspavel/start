4e3caa72b3ec504447365a49826ee827ebcea7a7
laspavel <laspavel@gmail.com>
2021-02-16 21:13:30 +0200

    Add ss scrips

---
+++ roles/base-wrk/files/ssarea.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+##ssfull script
+DATE=$(date +%Y%m%d-%H%M%S)
+gnome-screenshot -a -f ~/tmp/$DATE.png
---
+++ roles/base-wrk/files/ssfull.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+##ssfull script
+DATE=$(date +%Y%m%d-%H%M%S)
+gnome-screenshot -f ~/tmp/$DATE.png
