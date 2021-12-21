6960dad5ebb0964016930dc04302bc56770caa48
laspavel <laspavel@gmail.com>
2021-12-21 21:12:51 +0200

    Update Dash-to-panel-config

--- scripts/upgrade_fedora.sh
+++ scripts/upgrade_fedora.sh
@@ -2,6 +2,12 @@
 
 sudo dnf -y update
 sudo dnf -y upgrade --refresh
-sudo dnf -y install dnf-plugin-system-upgrade
-sudo dnf -y system-upgrade download --releasever=33
+sudo dnf -y install dnf-plugin-system-upgrade --best
+sudo dnf -y system-upgrade download --releasever=35
 sudo dnf system-upgrade reboot
+
+### After reboot ###
+sudo rpm --rebuilddb
+sudo dnf distro-sync --setopt=deltarpm=0
+sudo dnf install rpmconf
+sudo rpmconf -a
