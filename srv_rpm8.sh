f7535e7abd3629b3db4d3925d9bf5c851552c438
laspavel <laspavel@gmail.com>
2023-12-27 09:04:51 +0200

    Update rpm8 scripts

--- srv_rpm8.sh
+++ srv_rpm8.sh
@@ -4,5 +4,6 @@
 
 yum install -y epel-release
 yum install -y python3 python3-pip
-yum install -y ansible
+yum install -y ansible-core
+ansible-galaxy collection install -r plays/requirements.yml -p ./.collections
 ansible-playbook plays/srv_rpm8.yml
