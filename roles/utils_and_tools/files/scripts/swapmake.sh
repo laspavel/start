7bb3febba37b384a4fb224ae3a97e859f8903b0f
laspavel <laspavel@gmail.com>
2024-07-16 20:52:15 +0300

    Add VBox and swapmake scripts

--- roles/utils_and_tools/files/scripts/swapmake.sh
+++ roles/utils_and_tools/files/scripts/swapmake.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-# Add swapfile size 1Gb.
+SWAPFILE=/swapfile
+SWAPSIZE=$1
 
-dd if=/dev/zero of=/swapfile bs=1024 count=1048576
-chmod 600 /swapfile
-mkswap /swapfile
-swapon /swapfile
+calculat() { awk "BEGIN{ print $* }" ;}
+
+SWAPSIZEBYTE=$(calculat $SWAPSIZE*1048576)
+dd if=/dev/zero of=$SWAPFILE bs=1024 count=$SWAPSIZEBYTE
+chmod 600 $SWAPFILE
+mkswap $SWAPFILE
+swapon $SWAPFILE
 swapon --show
+
+# Проверяем, есть ли запись о swapfile в /etc/fstab
 
 # If required add line in /etc/fstab:
 # /swapfile swap swap defaults 0 0
 
-# info:
-# swapon --show
-
+grep -q "$SWAPFILE" /etc/fstab
+if [ $? -ne 0 ]; then
+    # Добавляем запись в /etc/fstab
+    echo "$SWAPFILE none swap sw 0 0" | tee -a /etc/fstab
+fi
